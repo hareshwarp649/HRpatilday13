c7a791bc4f2978ca1a94e39dcb95e7d5abface82
Harry <hareshwarp649>
2022-03-04 09:48:21 +0530

    Extend the max method max to std out

---
+++ MaxGeneric7/MethodGeniNum.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExteNumGen7
+{
+    internal class MethodGeniNum<T> where T:IComparable
+    {
+
+        public T[] GenValue;
+
+       
+        public MethodGeniNum(T[] GenValue)
+        {
+            this.GenValue = GenValue;
+        }
+
+        public T[] Sort(T[] GenValue)
+        {
+            Array.Sort(GenValue);
+            Array.Reverse(GenValue);
+            return GenValue;
+        }
+
+        //Generic method to find max value (UC4)
+        public T MaxValue(T[] GenValue)
+        {
+            var sortedValues = Sort(GenValue);
+            return sortedValues.ElementAt(0);
+        }
+
+        //Generic method to print max value (UC5)
+        public void PrintMaxValue()
+        {
+            var maxValue = MaxValue(this.GenValue);
+            Console.WriteLine("Maximum value from given number is : {0}", maxValue);
+        }
+
+        //Extend the max method to take more then three parameters(UC4)
+        public T ShowMax()
+        {
+            var maxValue = MaxValue(this.GenValue);
+            return maxValue;
+        }
+    }
+}
---
+++ MaxGeneric7/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExteNumGen7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome To The Finding Maximum Num Using Generic");
+
+            while (true)
+            {
+                Console.WriteLine("1: Find Max Of Three Int Number" + "\n2: Find Max Of Three Float Number" + "\n3: Find the max of three string number" + "\n4: Exit");
+
+                Console.Write("Enter A Choice From Above : ");
+                int select = int.Parse(Console.ReadLine());
+
+                switch (select)
+                {
+                    case 1:
+                        
+                        int[] intArray = {59,56,28,49 };
+                        MethodGeniNum<int> resIntNum = new MethodGeniNum<int>(intArray);
+                        resIntNum.PrintMaxValue();
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        
+                        float[] floatArray = { 36.4f, 58.45f, 49.2f, 69.58f };
+                        MethodGeniNum<float> resFtNum = new MethodGeniNum<float>(floatArray);
+                        resFtNum.PrintMaxValue();
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        
+                        string[] stringArray = { "Navbharat", "Navkranti", "Navswaraj", "Navdrushti" };
+                        MethodGeniNum<string> resStr = new MethodGeniNum<string>(stringArray);
+                        resStr.PrintMaxValue();
+                        Console.ReadLine();
+                        break;
+                    case 4:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Choose a right option");
+                        break;
+
+                }
+            }
+             
+        }
+    }
+}
