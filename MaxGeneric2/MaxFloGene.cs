4b2cb23e3ccc14ec6b590a48a62ec622bfcf8fd8
Harry <hareshwarp649>
2022-03-04 07:53:51 +0530

    Given float value Find The Maximum

---
+++ MaxGeneric2/MaxFloGene.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxNumGen2
+{
+    internal class MaxFloGene
+    {
+
+        public static int MaxNum2(int Value1, int Value2, int Value3)
+        {
+            if (Value1.CompareTo(Value2) >= 0 && Value1.CompareTo(Value3) >= 0)
+            {
+                return Value1;
+            }
+            else if (Value2.CompareTo(Value1) >= 0 && Value2.CompareTo(Value3) >= 0)
+            {
+                return Value2;
+            }
+            else if (Value3.CompareTo(Value1) >= 0 && Value3.CompareTo(Value2) >= 0)
+            {
+                return Value3;
+            }
+            return default;
+        }
+
+        public static float FloMaxNum3(float Value1, float Value2, float Value3)
+        {
+            if (Value1.CompareTo(Value2) >= 0 && Value1.CompareTo(Value3) >= 0)
+            {
+                return Value1;
+            }
+            else if (Value2.CompareTo(Value1) >= 0 && Value2.CompareTo(Value3) >= 0)
+            {
+                return Value2;
+            }
+            else if (Value3.CompareTo(Value1) >= 0 && Value3.CompareTo(Value2) >= 0)
+            {
+                return Value3;
+            }
+            return default;
+        }
+    }
+}
---
+++ MaxGeneric2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxNumGen2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+                
+                Console.WriteLine("Welcome To The Finding Maximum Num Using Generic");
+
+                while (true)
+                {
+                Console.WriteLine("1: Find Max Of Three Int Number" + "\n2: Find Max Of Three Float Number" + "\n3: Exit");
+
+                    Console.Write("Enter A Choice From Above : ");
+                    int select = int.Parse(Console.ReadLine());
+                    switch (select)
+                    {
+                        case 1:
+                            
+                            int val1 = 99, val2 = 44, val3 = 49;
+                            int resIntNum = MaxFloGene.MaxNum2(val1,val2,val3);
+                            Console.WriteLine("{0} is maximum value from first: {1} second: {2} third: {3}", resIntNum, val1,val2,val3);
+                            Console.ReadLine();
+                            break;
+                        case 2:
+                            
+                            float fval1 = 25.2f, fval2 = 5.4f, fval3 = 24.2f;
+                            float resFtNum = MaxFloGene.FloMaxNum3(fval1,fval2,fval3);
+                            Console.WriteLine("{0} is maximum value from first: {1} second: {2} third: {3}", resFtNum,fval1,fval2,fval3);
+                            Console.ReadLine();
+                            break;
+                        case 3:
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("Choose a right option");
+                            break;
+                    }
+                }
+        }
+    }
+}
